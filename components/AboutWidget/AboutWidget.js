45c996edfc3e45a007ee4c932b31fa045337f90d
rygim <ry.gimmy@gmail.com>
2017-06-04 21:27:20 -0400

    update about widget

--- components/AboutWidget/AboutWidget.js
+++ components/AboutWidget/AboutWidget.js
@@ -9,8 +9,21 @@ class AboutWidget extends React.Component {
         <div className="col-md-12">
           <div className="media">
             <div className="media-body">
-              <h5 className="mt-0">Ryan Gimmy</h5>
-              Ryan has been developing software since he was in high school and has over 10 year of experience developing software commercially. He considers himself a maker with a software engineer and likes to build things in addition to programming them.
+              <div className="row">
+                 <div className="col-md-4">
+                  <img className="rounded" src='http://www.gravatar.com/avatar/1b938f3a9ab80e8315f8aff6f2fc9950' />
+                 </div>
+                 <div className="col-md-8">
+                   <h5 className="mt-0">Ryan Gimmy</h5>
+                   <small>Co-Founder <a href='http://visallo.com'>Visallo</a>, Senior Software Engineer</small>
+                 </div>
+              </div>
+             <br />
+              <div className="row">
+                 <div className="col-md-12">
+              Ryan has been developing software since he was in high school and has over 10 years of experience developing software. He considers himself a maker with a specialty in software and data engineering and likes to build things in addition to programming them.
+                 </div>
+              </div>
             </div>
           </div>
         </div>
